5b5d7db2f7a38498914b31ae0e01d600a5af8590
Ashutosh <ashutosh.kumar.patel.2003@gmail.com>
2023-11-22 00:51:27 +0530

    Day 1

---
+++ Day1/twoSum.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = solution.twoSum(nums, target);
+    for (int i : result) {
+        cout << i << " ";
+    }
+    return 0;
+}
