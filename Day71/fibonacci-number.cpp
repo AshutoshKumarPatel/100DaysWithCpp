0f8d358eba160a64a680ca82fa7045a28b5c869d
Ashutosh <ashutosh.kumar.patel.2003@gmail.com>
2024-01-31 11:41:05 +0530

    Day 71

---
+++ Day71/fibonacci-number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int fib(int n) {
+        int f[n+1];
+        if (n == 1 || n == 0) return n;
+        f[0] = 0;
+        f[1] = 1; 
+        for (int i=2; i<=n; i++) {
+            f[i] = f[i-1] + f[i-2];
+        }
+        return f[n];
+    }
+};
+
+int main() {
+    int n = 10;
+    Solution sol;
+    cout << sol.fib(n) << endl;
+    return 0;
+}
