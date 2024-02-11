eea3762eebd7297c532ecbdb9f038601fea58a02
Ashutosh <ashutosh.kumar.patel.2003@gmail.com>
2024-02-11 23:57:53 +0530

    Day 84 Updated

--- Day84/letter-combinations-of-a-phone-number.cpp
+++ Day84/letter-combinations-of-a-phone-number.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 #include <algorithm>
 using namespace std;
 
 class Solution {
 public:
-    vector<string> letterCombinations(string digits) {
-        vector<vector<char>> nMapping = {{'a','b','c'}, {'d','e','f'}, {'g','h','i'}, {'j','k','l'}, {'m','n','o'}, {'p','q','r','s'}, {'t','u','v'}, {'w','x','y','z'}};
-        vector<string> ans;
-        for (int i = 0; i < digits.length(); i++) {
-            
+    void solve(int idx, string s, string temp,unordered_map<char,string> dialpad,vector<string> &result){
+        if(idx>=s.size()){
+            result.push_back(temp);
         }
+        char ch=s[idx];
+        string str=dialpad[ch];
+        
+        for(int i=0;i<str.length();i++){
+            temp.push_back(str[i]);
+            solve(idx+1, s, temp,dialpad, result);
+            temp.pop_back();
+        }
+    }
+    
+    vector<string> letterCombinations(string digits) {
+        if(digits.length()==0) return {};
+        vector<string> result;
+        unordered_map<char,string> dialpad;
+        dialpad['2']="abc";
+        dialpad['3']="def";
+        dialpad['4']="ghi";
+        dialpad['5']="jkl";
+        dialpad['6']="mno";
+        dialpad['7']="pqrs";
+        dialpad['8']="tuv";
+        dialpad['9']="wxyz";
+        solve(0,digits,"",dialpad,result);
+        return result;
     }
 };
 
